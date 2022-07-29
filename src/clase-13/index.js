c4b8bbfb73f54a7f52bf14ac23f57e92d55a6c58
Andres Badillo <r.andres.badillo@gmail.com>
2022-07-29 08:44:47 -0500

    Clases 11, 12 y 13

---
+++ src/clase-13/index.js
@@ -0,0 +1,20 @@
+// h1 { color: red ..... }
+// .parrafito { ... }
+// #pid { ... }
+
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+//btn.addEventListener('click', sumarInputVaues);
+form.addEventListener('submit', sumarInputVaues);
+
+function sumarInputVaues(event) {
+    console.log({event});
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = 'Resultado: ' + sumaInputs;
+}
